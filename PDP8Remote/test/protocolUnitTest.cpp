d99200f1b59c7cefc5a32504a05500c04979a57f
Mattis Lind <mattis@mattisborgen.se>
2020-03-27 07:33:55 +0100

    Added unit test case

---
+++ PDP8Remote/test/protocolUnitTest.cpp
@@ -0,0 +1,13 @@
+/*
+
+ Compile using c++ protcolUnitTest.cpp ../PDP8Server/protocol.cpp -o protocolUnitTest
+
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include "../PDP8Server/protocol.h"
+
+int main () {
+
+}
