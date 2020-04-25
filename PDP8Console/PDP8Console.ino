4a771d020839261447e5f15875967ac29f4844ac
Mattis Lind <mattis@mattisborgen.se>
2020-04-25 15:43:08 +0200

    Added Rolands copyright notice and have the correct atribution for his work.

--- PDP8Console/PDP8Console.ino
+++ PDP8Console/PDP8Console.ino
@@ -1,3 +1,23 @@
+/*
+
+Parts of this file is derived from original work by Roland Huismann. In particular those portions involved in commiunicating with the underlaying PDP-8 hardware
+which is taken directly from his project.
+
+For these portiosn this copyright apply:
+
+Copyright 2019 Roland Huisman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this project and associated documentation files , to deal in the project without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the project, and to permit persons to whom the project is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the project.
+
+THE PROJECT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE PROJECT OR THE USE OR OTHER DEALINGS IN THE PROJECT.
+
+The remaining portions. Notably the command dispatching and command line processing is work by me, Mattis Lind.
+
+*/
+
+
 // =============================================================================================================  
 //                                         Includes
   #include <Wire.h>                             // I2C handler
